c0bd618abbcc28eba0689b9d8a4a23c311ff7f3d
danielVillalb <116323070+danielVillalb@users.noreply.github.com>
2023-06-24 17:23:53 -0300

    Update calculadora.js

--- calculadora.js
+++ calculadora.js
@@ -36,21 +36,19 @@ calcular=()=>{
   }
 
 
-    datosConcatenados=datosConcatenados2+((eval(datosPromedio)*promedio2)/100);
+
     document.getElementById("resultado").innerHTML=datosConcatenados;
     /*------reemplazo el valor dentro de la matriz---------*/
     console.log("matriz de datos: ",matrizDatos)
-    console.log("esto va a ser promedioi "+((eval(datosPromedio)*promedio2)/100));
-    console.log("datosPromedio "+(eval(datosPromedio)));
-    console.log("Promedio "+(promedio2));
-    console.log("necesario "+necesario);
+
+
+
 }
 borrar=()=>{
     datosConcatenados=""
     datosConcatenados2=""
-    promedio="";
-    promedio2="";
-    datosPromedio="";
+  
+  
     document.getElementById("resultado").innerHTML="";
     document.getElementById("resultado2").innerHTML = "";
     for(let i=0;i<iteracion;i++){
